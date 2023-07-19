cb154ad04ffb422c122fbe568b6ec62641062151
jyoti pal <paljyoti0129@gmail.com>
2023-07-19 10:24:15 +0530

    link updated

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -128,7 +128,7 @@ const Contact = () => {
           </div>
         </div>
         <div className="flex justify-center py-12">
-          <Link to="/">
+          <Link to="/about">
             <a>
               <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
                 <HiOutlineChevronDoubleUp
